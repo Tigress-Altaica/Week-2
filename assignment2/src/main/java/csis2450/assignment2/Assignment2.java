9b13e428df01d178f69417460d6597d04d65bcd7
Anneliese Braunegg <akb89@case.edu>
2021-01-30 21:00:36 -0500

    Updated Assignment 2.java to include printing to the console

--- assignment2/src/main/java/csis2450/assignment2/Assignment2.java
+++ assignment2/src/main/java/csis2450/assignment2/Assignment2.java
@@ -76,6 +76,127 @@ public class Assignment2 {
 	}
 	
 	/**
+	 * Write a temperatures report header to the console.
+	 */
+	private static final void writeReportHeaderToConsole() {
+		
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("December 2020: Temperatures in Utah");
+		System.out.println("--------------------------------------------------------------");
+	}
+	
+	/**
+	 * Write the temperature highs, lows, and variances to the console
+	 * based on the data in the specified temperatures array. Also include
+	 * a header for the data being written.
+	 * 
+	 * @param tempsArray: The temperatures array, formatted such that each
+	 * 	row represents a day, the first column of each row stores the
+	 *  number of the day (in the month), the second column of each row
+	 *  stores the highest temperature of the day, and the third column
+	 *  stores the lowest temperature of the day
+	 */
+	private static final void writeHighLowVarianceToConsole(
+		int[][] tempsArray) {
+		
+		assert tempsArray != null;
+		assert tempsArray.length > 0;
+		
+		System.out.println("Day   High  Low   Variance");
+		System.out.println("--------------------------------------------------------------");
+		
+		int variance = 0;
+		
+		for (int i = 0; i < tempsArray.length; i++) {
+			variance = tempsArray[i][1] - tempsArray[i][2];
+			System.out.printf("%2d    %2d    %2d    %2d"
+				+ System.lineSeparator(),
+				(i + 1), tempsArray[i][0], tempsArray[i][1],
+				tempsArray[i][2], variance);
+		}
+	}
+	
+	/**
+	 * Write a temperatures summary to the console based on the data in
+	 * the specified temperatures array. Also include a header for the
+	 * data being written.
+	 * 
+	 * @param tempsArray: The temperatures array, formatted such that each
+	 * 	row represents a day, the first column of each row stores the
+	 *  number of the day (in the month), the second column of each row
+	 *  stores the highest temperature of the day, and the third column
+	 *  stores the lowest temperature of the day
+	 */
+	private static final void writeSummaryToConsole(int[][] tempsArray) {
+		
+		int indexOfHighestTemp = indexOfHighestTemp(tempsArray);
+		int highestDay = tempsArray[indexOfHighestTemp][0];
+		int highestValue = tempsArray[indexOfHighestTemp][1];
+		int averageHigh = averageHigh(tempsArray);
+		
+		int indexOfLowestTemp = indexOfLowestTemp(tempsArray);
+		int lowestDay = tempsArray[indexOfLowestTemp][0];
+		int lowestValue = tempsArray[indexOfLowestTemp][2];
+		int averageLow = averageLow(tempsArray);
+		
+		System.out.println("--------------------------------------------------------------");
+		System.out.printf("December Highest Temperature: 12/%d: %d Average"
+			+ " Hi: %.1f" + System.lineSeparator(),
+			highestDay, highestValue, averageHigh);
+		System.out.printf("December Lowest Temperature: 12/%d: %d Average"
+			+ " Lo: %.1f" + System.lineSeparator(),
+			lowestDay, lowestValue, averageLow);
+		System.out.println("--------------------------------------------------------------");
+	}
+	
+	/**
+	 * Write a temperatures graph to the console based on the data in the
+	 * specified temperatures array. Also include a header for the data
+	 * being written.
+	 * 
+	 * @param tempsArray: The temperatures array, formatted such that each
+	 * 	row represents a day, the first column of each row stores the
+	 *  number of the day (in the month), the second column of each row
+	 *  stores the highest temperature of the day, and the third column
+	 *  stores the lowest temperature of the day
+	 */
+	private static final void writeGraphToConsole(int[][] tempsArray) {
+		
+		System.out.println("Graph");
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("      1   5    10   15   20   25   30   35   40   45   50");
+		System.out.println("      |   |    |    |    |    |    |    |    |    |    |");
+		System.out.println("--------------------------------------------------------------");
+		
+		String pluses;
+		String minuses;
+		for (int i = 0; i < tempsArray.length; i++) {
+			
+			pluses = StringUtils.repeat("+", tempsArray[i][1]);
+			minuses = StringUtils.repeat("-", tempsArray[i][2]);
+			
+			if (i < 10) {
+				System.out.printf("%d  Hi %s" + System.lineSeparator(),
+					(i + 1), pluses);
+				System.out.printf("%d  Lo %s" + System.lineSeparator(),
+					(i + 1), minuses);
+			}
+			else {
+				System.out.printf("%d Hi %s" + System.lineSeparator(),
+					(i + 1), pluses);
+				System.out.printf("%d Lo %s" + System.lineSeparator(),
+					(i + 1), minuses);
+			}
+		}
+		
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("      |   |    |    |    |    |    |    |    |    |    |");
+		System.out.println("      1   5    10   15   20   25   30   35   40   45   50");
+		System.out.println("--------------------------------------------------------------");
+	}
+
+	
+	/**
 	 * Use the specified PrintWriter a temperatures report header to a
 	 * .txt file.
 	 * 
@@ -169,6 +290,59 @@ public class Assignment2 {
 		printWriter.printf("December Lowest Temperature: 12/%d: %d Average"
 			+ " Lo: %.1f" + System.lineSeparator(),
 			lowestDay, lowestValue, averageLow);
+		printWriter.println("--------------------------------------------------------------");
+	}
+	
+	/**
+	 * Use the specified PrintWriter to write a temperatures graph to
+	 * a .txt file based on the data in the specified temperatures array.
+	 * Also include a header for the data being written.
+	 * 
+	 * NOTE: The output file must already be specified within the
+	 * 	PrintWriter.
+	 * 
+	 * NOTE 2: This method does not close the PrintWriter.
+	 * 
+	 * @param tempsArray: The temperatures array, formatted such that each
+	 * 	row represents a day, the first column of each row stores the
+	 *  number of the day (in the month), the second column of each row
+	 *  stores the highest temperature of the day, and the third column
+	 *  stores the lowest temperature of the day
+	 * @param printWriter: The PrintWriter
+	 */
+	private static final void writeGraphToFile(
+		int[][] tempsArray, PrintWriter printWriter) {
+		
+		printWriter.println("Graph");
+		printWriter.println("--------------------------------------------------------------");
+		printWriter.println("      1   5    10   15   20   25   30   35   40   45   50");
+		printWriter.println("      |   |    |    |    |    |    |    |    |    |    |");
+		printWriter.println("--------------------------------------------------------------");
+		
+		String pluses;
+		String minuses;
+		for (int i = 0; i < tempsArray.length; i++) {
+			
+			pluses = StringUtils.repeat("+", tempsArray[i][1]);
+			minuses = StringUtils.repeat("-", tempsArray[i][2]);
+			
+			if (i < 10) {
+				printWriter.printf("%d  Hi %s" + System.lineSeparator(),
+					(i + 1), pluses);
+				printWriter.printf("%d  Lo %s" + System.lineSeparator(),
+					(i + 1), minuses);
+			}
+			else {
+				printWriter.printf("%d Hi %s" + System.lineSeparator(),
+					(i + 1), pluses);
+				printWriter.printf("%d Lo %s" + System.lineSeparator(),
+					(i + 1), minuses);
+			}
+		}
+		
+		printWriter.println("--------------------------------------------------------------");
+		printWriter.println("      |   |    |    |    |    |    |    |    |    |    |");
+		printWriter.println("      1   5    10   15   20   25   30   35   40   45   50");
 		printWriter.println("--------------------------------------------------------------");
 	}
 	
@@ -288,59 +462,6 @@ public class Assignment2 {
 	
 		return lowsSum / tempsArray.length;
 	}
-	
-	/**
-	 * Use the specified PrintWriter to write a temperatures graph to
-	 * a .txt file based on the data in the specified temperatures array.
-	 * Also include a header for the data being written.
-	 * 
-	 * NOTE: The output file must already be specified within the
-	 * 	PrintWriter.
-	 * 
-	 * NOTE 2: This method does not close the PrintWriter.
-	 * 
-	 * @param tempsArray: The temperatures array, formatted such that each
-	 * 	row represents a day, the first column of each row stores the
-	 *  number of the day (in the month), the second column of each row
-	 *  stores the highest temperature of the day, and the third column
-	 *  stores the lowest temperature of the day
-	 * @param printWriter: The PrintWriter
-	 */
-	private static final void writeGraphToFile(
-		int[][] tempsArray, PrintWriter printWriter) {
-		
-		printWriter.println("Graph");
-		printWriter.println("--------------------------------------------------------------");
-		printWriter.println("      1   5    10   15   20   25   30   35   40   45   50");
-		printWriter.println("      |   |    |    |    |    |    |    |    |    |    |");
-		printWriter.println("--------------------------------------------------------------");
-		
-		String pluses;
-		String minuses;
-		for (int i = 0; i < tempsArray.length; i++) {
-			
-			pluses = StringUtils.repeat("+", tempsArray[i][1]);
-			minuses = StringUtils.repeat("-", tempsArray[i][2]);
-			
-			if (i < 10) {
-				printWriter.printf("%d  Hi %s" + System.lineSeparator(),
-					(i + 1), pluses);
-				printWriter.printf("%d  Lo %s" + System.lineSeparator(),
-					(i + 1), minuses);
-			}
-			else {
-				printWriter.printf("%d Hi %s" + System.lineSeparator(),
-					(i + 1), pluses);
-				printWriter.printf("%d Lo %s" + System.lineSeparator(),
-					(i + 1), minuses);
-			}
-		}
-		
-		printWriter.println("--------------------------------------------------------------");
-		printWriter.println("      |   |    |    |    |    |    |    |    |    |    |");
-		printWriter.println("      1   5    10   15   20   25   30   35   40   45   50");
-		printWriter.println("--------------------------------------------------------------");
-	}
 
 	/**
 	 * Main method for Assignment 2.
@@ -364,6 +485,12 @@ public class Assignment2 {
 		/* Instantiate a PrintWriter for writing to the report file */
 		PrintWriter printWriter = new PrintWriter(
 			Assignment2.reportFilePath);
+		
+		/* Write to the console */
+		writeReportHeaderToConsole();
+		writeHighLowVarianceToConsole(tempsArray);
+		writeSummaryToConsole(tempsArray);
+		writeGraphToConsole(tempsArray);
 		
 		/* Write to the report file */
 		writeReportHeaderToFile(printWriter);
